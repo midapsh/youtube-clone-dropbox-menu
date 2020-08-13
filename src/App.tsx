85f9edbb9235513b7a46e6a5d858c7e4eeb6045e
midapsh <34818053+midapsh@users.noreply.github.com>
2020-08-13 14:24:25 -0300

    Add base css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import HomePage from './page/HomePage';
+import GlobalStyles from './styles/GlobalStyles';
+
 const App: React.FC = () => {
-  return <div>asdasd</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <HomePage />
+    </>
+  );
 };
 
 export default App;
