3c4ff784c7e9be4513f0fc1173eec692651d6dd8
midapsh <34818053+midapsh@users.noreply.github.com>
2020-08-13 16:11:23 -0300

    Add first functionality to sideMenu

---
+++ src/components/SideMenu/constants.ts
@@ -0,0 +1,1 @@
+export const scrollThreshold = 300;
--- src/components/SideMenu/index.tsx
+++ src/components/SideMenu/index.tsx
@@ -1,18 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import clsx from 'clsx';
 
+import { scrollThreshold } from './constants';
 import { Container } from './styles';
 
 const SideMenu: React.FC = ({ children }) => {
+  const [scrollY, setScrollY] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+
   useEffect(() => {
     function onScroll() {
-      console.log(window.scrollY);
+      setScrollY(window.scrollY);
+      setIsActive(false);
     }
 
     window.addEventListener('scroll', onScroll);
     return () => window.removeEventListener('scroll', onScroll);
   }, []);
 
-  return <Container>{children}</Container>;
+  const classes = [
+    isActive ? 'open' : '',
+    scrollY <= scrollThreshold ? 'scrollOpen' : '',
+  ];
+  const className = clsx(classes);
+
+  return <Container className={className}>{children}</Container>;
 };
 
 export default SideMenu;
