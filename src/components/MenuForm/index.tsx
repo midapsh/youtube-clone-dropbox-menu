a38051327011de1c0e5316371341164c5d0b1588
midapsh <34818053+midapsh@users.noreply.github.com>
2020-08-13 16:32:14 -0300

    Add styles before Form style

--- src/components/MenuForm/index.tsx
+++ src/components/MenuForm/index.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Navigation, DropboxLogo, Form } from './styles';
 
 const MenuForm: React.FC = ({ children }) => {
-  return <Container>{children}</Container>;
+  return (
+    <Container>
+      <Navigation>
+        <h1>
+          <DropboxLogo />
+          <span>DropBox</span>
+        </h1>
+
+        <button className="action--close" type="button">
+          ✕
+        </button>
+      </Navigation>
+
+      <Form>
+        <span className="title">Registre-se</span>
+        <span className="subtitle">preencha o formulário abaixo</span>
+
+        <input type="text" placeholder="Nome" />
+        <input type="text" placeholder="Sobrenome" />
+        <input type="email" placeholder="E-mail" />
+        <input type="password" placeholder="Senha" />
+
+        <button type="button">Prosseguir</button>
+
+        <span className="terms">
+          Esta página está sujeita à Política de privacidade e aos Termos de
+          serviço.
+        </span>
+      </Form>
+      {children}
+    </Container>
+  );
 };
 
 export default MenuForm;
--- src/components/MenuForm/styles.ts
+++ src/components/MenuForm/styles.ts
@@ -1,3 +1,29 @@
 import styled from 'styled-components';
+import { FaDropbox } from 'react-icons/fa';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+
+  @media (min-width: 1024px) {
+    max-width: 480px;
+  }
+`;
+
+export const Navigation = styled.nav`
+  display: flex;
+
+  align-items: center;
+  justify-content: space-between;
+
+  padding: 16px;
+`;
+
+export const DropboxLogo = styled(FaDropbox)`
+  width: 36px;
+  height: 32px;
+  fill: var(--color-blue);
+`;
+
+export const Form = styled.div``;
