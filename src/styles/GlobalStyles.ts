cbc85edb55d82e84afdf1782b17b3b94103e8806
midapsh <34818053+midapsh@users.noreply.github.com>
2020-08-13 14:26:59 -0300

    Add colors

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -9,4 +9,14 @@ export default createGlobalStyle`
   *, button, input {
     font-family: Roboto, sans-serif;
   }
+
+  :root {
+    --color-primary: #0d2f81;
+    --color-secondary: #ffe7aa;
+    --color-tertiary: #fff;
+    --color-quaternary: #000;
+
+    --color-blue: #0061ff;
+    --color-border: #bdc4c9;
+  }
 `;
